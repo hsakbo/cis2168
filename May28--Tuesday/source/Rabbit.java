ded4fc665bd99b60e096baf70d3e175b029e0ba1
silverflash <hsa310@gmail.com>
2019-05-30 06:09:13 +0900

    bleh

--- May28--Tuesday/source/Rabbit.java
+++ May28--Tuesday/source/Rabbit.java
@@ -10,7 +10,7 @@ public class Rabbit extends Animal {
 
     private static final int S_R = 2;
     private static int quadrant;
-    private static int sigsafe = 0;
+
 
     private int goSafePlace(){
 	//rows and cols = 20
@@ -67,11 +67,12 @@ public class Rabbit extends Animal {
 
 
 	int dir, bushLen = 21;
+	dir = Model.STAY;
 	for(int i = 0; i < 8; i++)
 	    if(look(i) == Model.BUSH)
 		if(distance(i) < bushLen)
 		    {
-			bushLen = distance(i);
+			bushLen = distance(i) -1;
 			dir = i;
 		    }
 
@@ -90,125 +91,146 @@ public class Rabbit extends Animal {
 	    }
 	
 
+
+
 	if(bushLen == 0)
-	    int retVal = parking();		
+	    {		
+		return parking(dir);
+	    }
+        
 	
-	return Model.STAY;
+	return dir;
     }
 
-    private int parking(){
-
-	int dir, tempV1, tempV2;
+    private int parking(int dir){
+	int tempV1, tempV2;
 	int count = 0;
 	for(int i=0; i<8; i++)
-	    if(look(i) == Model.BUSH){
+	    if(look(i) == Model.BUSH)
 		count++;
-		dir = i;
-	    }
+		
 	if (count < 5){
+	    
+	    switch (dir){
+	    case Model.N:
+		tempV1 = look(Model.NE);
+		tempV2 = look(Model.NW);
 
-	switch (dir){
-	case Model.N:
-	    tempV1 = look(Model.NE);
-	    tempV2 = look(Model.NW);
-
-	    if((tempV1 == Model.BUSH) && (tempV2 == Model.BUSH))
-		{
-		    if (canMove(Model.E))
-			return Model.E;
-		    else if(canMove(Model.W))
-			return Model.W;
-		}
-	    break;
-	case Model.NE:
-	    if(canMove(Model.N) && canMove(Model.E))
-		{
-	    if (canMove(Model.N))
-		return 1;
-	    else
-		return 3;
-		}
-	    break;
-	case Model.E:
-	    tempV1 = look(Model.NE);
-	    tempV2 = look(Model.SE);
-	    if((tempV1 == Model.BUSH) && (tempV2 == Model.BUSH))
-		{
-		    if(canMove(Model.N))
-			return Model.N;
-		    else if (canMove(Model.S))
-			return Model.S;
+		if((tempV1 == Model.BUSH) && (tempV2 == Model.BUSH))
+		    {
+			if (canMove(Model.E))
+			    return Model.E;
+			else if(canMove(Model.W))
+			    return Model.W;
+		    }
+		break;
+	    case Model.NE:
+		if(canMove(Model.N) && canMove(Model.E))
+		    {
+		
+			if (canMove(Model.N)){
+			   
+			    return Model.N;
+			}
+			else
+			    return Model.E;
+		    }
+		break;
+	    case Model.E:
+		tempV1 = look(Model.NE);
+		tempV2 = look(Model.SE);
+		if((tempV1 == Model.BUSH) && (tempV2 == Model.BUSH))
+		    {
 		    
-		}
-	    break;
-	case Model.SE:
-	    if(canMove(Model.E) && canMove(Model.S)){
-		if (canMove(Model.E))
-		    return Model.E;
-		else
-		    return Model.S;
-	    }
-	    break;
-	case Model.S:
-	    tempV1 = look(Model.SE);
-	    tempV2 = look(Model.SW);
 
-	    if((tempV1 == Model.BUSH) && (tempV2 == Model.BUSH))
-		{
+			if(canMove(Model.N))
+			    return Model.N;
+			else if (canMove(Model.S))
+			    return Model.S;
+		    }
+		break;
+	    case Model.SE:
+		if(canMove(Model.E) && canMove(Model.S)){
 		    if (canMove(Model.E))
 			return Model.E;
 		    else
-			return Model.W;
-		}
-	    break;
-	case Model.SW:
-	    if (canMove(Model.S) && canMove(Model.W)){
-		if (canMove(Model.S))
-		    return Model.S;
-		else
-		    return Model.W;
-	    }
-	    break;
-	case Model.W:
-	    tempV1 = look(Model.NW);
-	    tempV2 = look(Model.SW);
-
-	    if((tempV1 == Model.BUSH) && (tempV2 == Model.BUSH))
-		{
-		    if (canMove(Model.N))
-			return Model.N;
-		    else
 			return Model.S;
 		}
-	    break;
-	case Model.NW:
-	    if(canMove(Model.N) && canMove(Model.W)){
-		    if(canMove(Model.N))
-		       return Model.N;
+		break;
+	    case Model.S:
+		tempV1 = look(Model.SE);
+		tempV2 = look(Model.SW);
+
+		if((tempV1 == Model.BUSH) && (tempV2 == Model.BUSH))
+		    {
+			if (canMove(Model.E))
+			    return Model.E;
+			else
+			    return Model.W;
+		    }
+		break;
+	    case Model.SW:
+		if (canMove(Model.S) && canMove(Model.W)){
+		    if (canMove(Model.S))
+			return Model.S;
 		    else
 			return Model.W;
 		}
-	    break;
+		break;
+	    case Model.W:
+		tempV1 = look(Model.NW);
+		tempV2 = look(Model.SW);
+
+		if((tempV1 == Model.BUSH) && (tempV2 == Model.BUSH))
+		    {
+			if (canMove(Model.N))
+			    return Model.N;
+			else
+			    return Model.S;
+		    }
+		break;
+	    case Model.NW:
+		if(canMove(Model.N) && canMove(Model.W)){
+		    if(canMove(Model.N))
+			return Model.N;
+		    else
+			return Model.W;
+		}
+		break;
 	
 		       
-	}
+	    }
 	}
 
 	else if (count < 1 && count >=5)
 	    return Model.STAY; //give up and die
 
-        this.sigsafe = 1;
+        
 	return Model.STAY;
+	
+    }
+
+    private int escape(){
 	
     }
     
     int decideMove(){
 
-	if(this.sigsafe == 0)
-	    int a = goSafePlace();
+	int dist = 1;
+	
+	for(int i = 0; i < 8; i++)
+	    if(look(i) == Model.FOX)
+	    dist = distance(i) - 1;
 
+	if (dist == 0)
+	    return escape(); 
+	
+	
+	return goSafePlace();
+	
+	//System.out.println(sigsafe);
 	
 	//return random(Model.MIN_DIRECTION, Model.MAX_DIRECTION);
-	return Model.STAY;
+	
     }
 }
