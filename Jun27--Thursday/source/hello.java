4761ea615527ee21b5feab0c6fd627ccda644ddf
silverflash <hsa310@gmail.com>
2019-06-27 12:06:05 +0900

    update

---
+++ Jun27--Thursday/source/hello.java
@@ -0,0 +1,21 @@
+public class hello{
+
+    public static void main(String[] args){
+
+	//heaps. (root is the smallest)
+	/*
+	parent to left child in array (2p+1) (p is 0 at root)
+	  parent to right child in array(2p+2) (p is 0 at root).
+	  child to parent is (c-1)/2 (integer division floor)
+
+	  
+	  insertion: while(parent >= 0 and table[parent] > table[child])
+	  removal (ONLY ROOT): last index becomes root. root then goes down.
+
+	  priority queue: the smallest item is always the root.
+
+
+	*/
+	  }
+
+}
